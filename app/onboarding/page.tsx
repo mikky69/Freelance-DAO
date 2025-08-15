1b64fefde075c556c99b020b8b086c55eabe1686
John Kenechukwu <johnkenechukwu24@gmail.com>
2025-08-15 11:49:58 +0100

    Initial backend auth commit

--- app/onboarding/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import Web3Onboarding from "@/components/web3-onboarding"
-
-export default function OnboardingPage() {
-  return <Web3Onboarding onComplete={() => (window.location.href = "/dashboard")} />
-}
